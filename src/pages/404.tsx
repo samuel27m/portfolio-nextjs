e16b268f46744aa2c9759e79b4ea1f00181a4585
Samuel Caçador <samuelmoreira27@gmail.com>
2023-03-05 19:41:58 +0000

    fix: correctly add font

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,7 +1,7 @@
-import Image from "next/image";
+import Image from 'next/image';
 
 export default function Custom404() {
     return (
-        <Image className="img-fluid mx-auto d-block" src="/404.jpg" alt="404" width={900} height={500}  />
+        <Image className="img-fluid mx-auto d-block" src="/404.jpg" alt="404" width={900} height={500} />
     );
 }
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,14 +1,21 @@
 import type { AppProps } from 'next/app'
+import Head from 'next/head';
 import Header from '../components/header';
 import Footer from '../components/footer';
 
+import { Montserrat } from 'next/font/google';
 import 'bootstrap/scss/bootstrap.scss'
 import '../styles/global.scss'
-import Head from "next/head";
+
+const montserrat = Montserrat({
+  weight: ['400', '700'],
+  style: ['normal', 'italic'],
+  subsets: ['latin'],
+});
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <div>
+    <div className={montserrat.className}>
       <Head>
         <title>Samuel Caçador</title>
         <link rel="icon" href="/favicon.png" />
