d8e84c11e2dbc91290ba25e560d12a99f02b1763
Samuel Caçador <samuelmoreira27@gmail.com>
2024-11-24 01:42:00 +0000

    calculate experience years automatically

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -1,12 +1,17 @@
 import Link from 'next/link';
 
 export default function About() {
+    const now = new Date(2025,1);
+    const march2018 = new Date(2015, 2);
+    const experienceYears = now.getFullYear() - march2018.getFullYear()
+        - (now < new Date(now.getFullYear(), march2018.getMonth(), march2018.getDate()) ? 1 : 0);
+
     return (
         <div className="about">
             <p className="about-text">
                 I am a
                 <span className="text-red"> Full Stack Web Developer </span>
-                with +7 years of experience in developing applications for the world.
+                with +{experienceYears} years of experience in developing applications for the world.
                 I have been the lead developer in many projects across the years, and I am a <a target="_blank" rel="noopener noreferrer" href="https://www.youracclaim.com/badges/10eb31fe-bec4-418b-b445-293e6835fb51/">Magento 2 Professional Developer</a> who worked on many aspects of Magento customization and development.
                 Lately I&apos;ve getting into the world of JavaScript libraries and frameworks.
             </p>
